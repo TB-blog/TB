7b0a07005f6de0bde8e5467f6d47123f3dbe38eb
Trevor <xizhouh@gmail.com>
2018-04-26 17:05:35 -0500

    build: Update eslint config

--- src/entry-server.js
+++ src/entry-server.js
@@ -11,7 +11,7 @@ export default context => {
     const { fullPath } = router.resolve(url).route;
 
     if (fullPath !== url) {
-      return reject(new Error({ url: fullPath }));
+      return reject({ url: fullPath });
     }
 
     router.push(url);
@@ -19,7 +19,7 @@ export default context => {
     router.onReady(() => {
       const matchedComponents = router.getMatchedComponents();
       if (!matchedComponents.length) {
-        return reject(new Error({ code: 404 }));
+        return reject({ code: 404 });
       }
       Promise.all(matchedComponents.map(({ asyncData }) => asyncData && asyncData({
         store,
